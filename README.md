513d970e0559b31d6e4834a001049c8eb4ad6c89
Troy Harvey <troy.harvey@seven-26.com>
2015-09-13 01:31:09 -0400

    😾

---
+++ README.md
@@ -0,0 +1,40 @@
+# ✻ jwt
+
+`jwt` is an npm package for decoding [JSON Web Tokens](http://jwt.io/) (JWT) and the Unix timestamps within their bowels.
+
+## ✻ Installation
+
+```
+npm install -g jwt-cli
+```
+
+![](checkitout.gif)
+
+## ✻ Usage
+
+```
+$ jwt eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOlwvXC9leGFtcGxlLm9yZyIsImF1ZCI6Imh0dHA6XC9cL2V4YW1wbGUuY29tIiwiaWF0IjoxMzU2OTk5NTI0LCJuYmYiOjEzNTcwMDAwMDAsImV4cCI6MTQwNzAxOTYyOSwianRpIjoiaWQxMjM0NTYiLCJ0eXAiOiJodHRwczpcL1wvZXhhbXBsZS5jb21cL3JlZ2lzdGVyIiwidGVzdC10eXBlIjoiZm9vIn0.UGLFIRACaHpGGIDEEv-4IIdLfCGXT62X1vYx7keNMyc
+
+✻ Header
+{
+  "typ": "JWT",
+  "alg": "HS256"
+}
+
+✻ Payload
+{
+  "iss": "http://example.org",
+  "aud": "http://example.com",
+  "iat": 1356999524,
+  "nbf": 1357000000,
+  "exp": 1407019629,
+  "jti": "id123456",
+  "typ": "https://example.com/register",
+  "test-type": "foo"
+}
+   iat: 1356999524 Mon Dec 31 2012 19:18:44 GMT-0500 (EST)
+   nbf: 1357000000 Mon Dec 31 2012 19:26:40 GMT-0500 (EST)
+   exp: 1407019629 Sat Aug 02 2014 18:47:09 GMT-0400 (EDT)
+
+✻ Signature UGLFIRACaHpGGIDEEv-4IIdLfCGXT62X1vYx7keNMyc
+```
---
+++ bin/jwt.js
@@ -0,0 +1,48 @@
+#! /usr/bin/env node
+
+var colors = require('colors');
+var json = require('format-json');
+var jwt = require('jsonwebtoken');
+
+function niceDate(unixTimestamp) {
+    var date = new Date(unixTimestamp * 1000);
+    return colors.yellow(unixTimestamp) + " " + date.toLocaleString();
+}
+
+var token = {
+    'string': process.argv[2]
+};
+
+if (token.string === undefined || token.string.split('.').length !== 3) {
+    console.log('jwt-cli version 1.0.1\n');
+    console.log(colors.yellow('Usage:'));
+    console.log(' jwt [encoded token]');
+    return;
+}
+
+token.parts = token.string.split('.');
+token.decoded = jwt.decode(token.string, {complete: true});
+
+if (token.decoded === null) {
+    console.log('\n😾  token no good');
+    return;
+}
+
+console.log(
+    '\n' +
+    colors.green(token.parts[0]) + '.' +
+    colors.yellow(token.parts[1]) + '.' +
+    colors.red(token.parts[2])
+);
+
+console.log(colors.green('\n✻ Header'));
+console.log(colors.green(json.plain(token.decoded.header)));
+
+console.log(colors.yellow('\n✻ Payload'));
+console.log(colors.yellow(json.plain(token.decoded.payload)));
+
+console.log(colors.yellow('   iat: ') + niceDate(token.decoded.payload.iat));
+console.log(colors.yellow('   nbf: ') + niceDate(token.decoded.payload.nbf));
+console.log(colors.yellow('   exp: ') + niceDate(token.decoded.payload.exp));
+
+console.log(colors.red('\n✻ Signature ' + token.decoded.signature));
