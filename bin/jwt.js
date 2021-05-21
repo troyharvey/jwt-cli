30c7c4d6e19854b32bfe2134596dcedbba95bcbc
Troy Harvey <troyharvey@gmail.com>
2021-05-20 23:46:18 -0400

    Update npm packages

--- bin/jwt.js
+++ bin/jwt.js
@@ -1,60 +1,60 @@
 #! /usr/bin/env node
 
-var colors = require('colors/safe');
-var json = require('format-json');
-var jwt = require('jsonwebtoken');
-var parseArgs = require('minimist');
+var colors = require("colors/safe");
+var json = require("format-json");
+var jwt = require("jsonwebtoken");
+var parseArgs = require("minimist");
 
 function niceDate(unixTimestamp) {
   var dateString;
-  if (typeof unixTimestamp === 'number' && !isNaN(unixTimestamp)) {
+  if (typeof unixTimestamp === "number" && !isNaN(unixTimestamp)) {
     dateString = new Date(unixTimestamp * 1000).toLocaleString();
   } else {
-    dateString = 'Invalid Date';
+    dateString = "Invalid Date";
   }
-  return colors.yellow(unixTimestamp) + ' ' + dateString;
+  return colors.yellow(unixTimestamp) + " " + dateString;
 }
 
 function processToken(token) {
-  if (token.string === undefined || token.string.split('.').length !== 3) {
-    const pkg = require('../package.json');
+  if (token.string === undefined || token.string.split(".").length !== 3) {
+    const pkg = require("../package.json");
     console.log(`jwt-cli - JSON Web Token parser [version ${pkg.version}]\n`);
     console.info(
-      colors.yellow('Usage: jwt <encoded token> --secret=<signing secret>\n')
+      colors.yellow("Usage: jwt <encoded token> --secret=<signing secret>\n")
     );
-    console.log('ℹ Documentation: https://www.npmjs.com/package/jwt-cli');
+    console.log("ℹ Documentation: https://www.npmjs.com/package/jwt-cli");
     console.log(
-      '⚠ Issue tracker: https://github.com/troyharvey/jwt-cli/issues'
+      "⚠ Issue tracker: https://github.com/troyharvey/jwt-cli/issues"
     );
     return;
   }
 
-  token.parts = token.string.split('.');
+  token.parts = token.string.split(".");
   token.decoded = jwt.decode(token.string, { complete: true });
 
   if (token.decoded === null) {
-    console.log('\n😾  token no good');
+    console.log("\n😾  token no good");
     return false;
   }
 
-  console.log(colors.yellow('\nTo verify on jwt.io:'));
+  console.log(colors.yellow("\nTo verify on jwt.io:"));
   console.log(
-    '\n' +
-      colors.magenta('https://jwt.io/#id_token=') +
+    "\n" +
+      colors.magenta("https://jwt.io/#id_token=") +
       colors.cyan(token.parts[0]) +
-      '.' +
+      "." +
       colors.yellow(token.parts[1]) +
-      '.' +
+      "." +
       colors.magenta(token.parts[2])
   );
 
-  console.log(colors.cyan('\n✻ Header'));
+  console.log(colors.cyan("\n✻ Header"));
   console.log(colors.cyan(json.plain(token.decoded.header)));
 
-  console.log(colors.yellow('\n✻ Payload'));
+  console.log(colors.yellow("\n✻ Payload"));
   console.log(colors.yellow(json.plain(token.decoded.payload)));
 
-  const dates = { iat: 'Issued At', nbf: 'Not Before', exp: 'Expiration Time' };
+  const dates = { iat: "Issued At", nbf: "Not Before", exp: "Expiration Time" };
   for (const [field, name] of Object.entries(dates)) {
     if (Object.prototype.hasOwnProperty.call(token.decoded.payload, field)) {
       console.log(
@@ -63,16 +63,16 @@ function processToken(token) {
     }
   }
 
-  console.log(colors.magenta('\n✻ Signature ' + token.decoded.signature));
+  console.log(colors.magenta("\n✻ Signature " + token.decoded.signature));
   return true;
 }
 
 function verifyToken(token, secret) {
   try {
     jwt.verify(token.string, secret);
-    console.log(colors.green('\n✻ Signature Verified!'));
+    console.log(colors.green("\n✻ Signature Verified!"));
   } catch (err) {
-    console.log(colors.red('\n✻ Invalid Signature!'));
+    console.log(colors.red("\n✻ Invalid Signature!"));
   }
 }
 
@@ -86,17 +86,17 @@ function handleTokenAsAnArg() {
 }
 
 function handleTokenAsStdin() {
-  var data = '';
-  process.stdin.on('readable', function () {
+  var data = "";
+  process.stdin.on("readable", function () {
     var chunk;
     while ((chunk = process.stdin.read())) {
       data += chunk;
     }
   });
 
-  process.stdin.on('end', function () {
+  process.stdin.on("end", function () {
     // There will be a trailing \n from the user hitting enter. Get rid of it.
-    data = data.replace(/\n$/, '');
+    data = data.replace(/\n$/, "");
     token.string = data;
     processToken(token);
   });
