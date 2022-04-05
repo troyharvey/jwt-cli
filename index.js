16790f2f3a36ad1339335d24eb4112c43086fdbf
Troy Harvey <t.harvey@carta.com>
2022-04-04 23:31:02 -0400

    Add colorful console output with chalk

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const { createDecoder, createVerifier } = require("fast-jwt")
-var parseArgs = require('minimist')
+import { createDecoder, createVerifier } from 'fast-jwt'
+import parseArgs from 'minimist';
+import chalk from 'chalk';
+
 
 async function pipedToken(readable) {
   let result = "";
@@ -7,6 +9,12 @@ async function pipedToken(readable) {
     result += chunk;
   }
   return result;
+}
+
+function niceDate(unixTimestamp) {
+  if (typeof unixTimestamp === 'number' && !isNaN(unixTimestamp)) {
+    return new Date(unixTimestamp * 1000).toLocaleString();
+  }
 }
 
 function decodeToken(token, secret) {
@@ -24,9 +32,9 @@ async function getToken() {
   if (process.stdin.isTTY) {
     var argv = parseArgs(process.argv.slice(2));
     return argv["_"][0]
-  } else {
-    return await pipedToken(process.stdin);
   }
+
+  return await pipedToken(process.stdin);
 }
 
 function getArgument(key) {
@@ -34,23 +42,71 @@ function getArgument(key) {
   return argv[key]
 }
 
-function printToken(decodedToken, output) {
-  if (output == "json") {
-    console.log(JSON.stringify(decodedToken, null, 2));
-    return;
+function prettyJson(json) {
+  return JSON.stringify(json, null, 2);
+}
+
+function outputHeader(header) {
+  console.log(chalk.cyan("\n✻ Header"));
+  console.log(chalk.cyan(prettyJson(header)));
+}
+
+function outputPayload(payload) {
+  console.log(chalk.yellow("\n✻ Payload"));
+  console.log(chalk.yellow(prettyJson(payload)));
+
+}
+
+function outputSignature(signature) {
+  console.log(chalk.magenta(`\n✻ Signature ${signature}`));
+}
+
+function outputTokenAsJson(decodedToken) {
+  process.stdout.write(JSON.stringify(decodedToken, null, 2));
+}
+
+function outputJwtIoLink(token) {
+  const parts = token.split('.');
+  console.log(chalk.yellow('\nTo verify on jwt.io:'));
+  console.log(
+    '\n' +
+      chalk.magenta('https://jwt.io/#id_token=') +
+      chalk.cyan(parts[0]) +
+      '.' +
+      chalk.yellow(parts[1]) +
+      '.' +
+      chalk.magenta(parts[2])
+  );
+}
+
+function outputNicePayloadDates(payload) {
+  if (payload.iat) {
+    console.log(chalk.yellow(`   iat: ${payload.iat} `) + niceDate(payload.iat));
   }
 
-  console.log("\n✻ Header");
-  console.log(decodedToken.header);
+  if (payload.nbf) {
+    console.log(chalk.yellow(`   nbf: ${payload.nbf} `) + niceDate(payload.nbf));
+  }
 
-  console.log("\n✻ Payload");
-  console.log(decodedToken.payload);
+  if (payload.exp) {
+    console.log(chalk.yellow(`   exp: ${payload.exp} `) + niceDate(payload.exp));
+  }
 }
 
 (async() => {
-  token = await getToken();
-  secret = getArgument("secret");
-  output = getArgument("output");
-  decodedToken = decodeToken(token, secret);
-  printToken(decodedToken, output);
+  const token = await getToken();
+  const secret = getArgument("secret");
+  const output = getArgument("output");
+  const decodedToken = decodeToken(token, secret);
+
+  if (output == "json") {
+    outputTokenAsJson(decodedToken);
+  } else {
+    outputJwtIoLink(token);
+    outputHeader(decodedToken.header);
+    outputPayload(decodedToken.payload);
+    outputNicePayloadDates(decodedToken.payload);
+    outputSignature(decodedToken.signature);
+  }
+
 })();
