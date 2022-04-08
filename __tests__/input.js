11d20ccb9a9e5ad891b1988bb1854fc769008141
Troy Harvey <t.harvey@carta.com>
2022-04-08 00:48:42 -0400

    Run prettier on input tests

--- __tests__/input.js
+++ __tests__/input.js
@@ -13,27 +13,29 @@ test("get a token from stdin piped readable input stream", async () => {
 });
 
 test("get cli argument", () => {
-    expect(getArgument("output")).toBe(undefined);
-})
+  expect(getArgument("output")).toBe(undefined);
+});
 
 test("get token from cli argument", async () => {
-    process = {
-        "stdin": {
-            "isTTY": true
-        },
-        "argv": ["node", "index.js", "a.tok.en"]
-    }
-    const token = await getToken(process);
-    expect(token).toBe("a.tok.en");
+  process = {
+    stdin: {
+      isTTY: true,
+    },
+    argv: ["node", "index.js", "a.tok.en"],
+  };
+  const token = await getToken(process);
+  expect(token).toBe("a.tok.en");
 });
 
 test("get token from stdin piped stream", async () => {
-    process = {
-        "stdin": {
-            "isTTY": false
-        }
-    }
-    process.stdin = Readable.from(tokenGenerator());
-    const token = await getToken(process);
-    expect(token).toBe("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.bbnVJXHRSGcz5UbklFWC-_MCZQSucRVAwPfEbp5KoJ4");
+  process = {
+    stdin: {
+      isTTY: false,
+    },
+  };
+  process.stdin = Readable.from(tokenGenerator());
+  const token = await getToken(process);
+  expect(token).toBe(
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.bbnVJXHRSGcz5UbklFWC-_MCZQSucRVAwPfEbp5KoJ4"
+  );
 });
